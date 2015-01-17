20d2dcf6b8706a1e636c189437a5f71719da0cc3
Adrian Gschwend <ktk@netlabs.org>
2015-01-17 18:10:05 +0100

    Fixed Gulp, updated files, added tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
+/*jshint node:true, bitwise:true, curly:true, immed:true, indent:2, latedef:true, newcap:true, noarg: true, noempty:true, nonew:true, quotmark:single, undef:true, unused: true, trailing:true, white:false */
 
-"use strict";
+'use strict';
 var gulp = require('gulp'),
     request = require('request'),
     fs = require('fs'),
+    tape = require('tape'),
     compiler = require('./lib/compiler.js');
+//    uvc = require('./index.js');
 
 
-gulp.task('default', function(){
-  // place code for your default task here
-});
-
+gulp.task('default', ['fetch', 'compile', 'test']);
 
 /*
  * fetchrdfa: update the rdfa-context file from the W3C site
@@ -29,17 +29,30 @@ gulp.task('fetch', function(){
  * compile: generate the N3 and JSON-LD context from the Turtle files
  */
 gulp.task('compile', function(){
-  // place code for your default task here
   compiler.compile();
 });
 
 /*
- * test: 
+ * test: make sure it worked out properly
+     TODO add test for Turtle/Nt as well
  */
 gulp.task('test', function(){
-  // place code for your default task here
+//  console.log(uvc.prefixes);
+
+  var prefix = './rdf/uduvudu.jsonld';
+  var context;
+
+  fs.readFile(prefix, function (err, data) {
+    if (err) {
+      console.log('Failed to load '+prefix);
+    }
+    context = JSON.parse(data);
+    tape('check rdfa & uduvudu context', function (t) {
+      t.plan(2);
+      t.deepEqual(context['@context']['void'], 'http://rdfs.org/ns/void#');
+      t.deepEqual(context['@context']['hydra'], 'http://www.w3.org/ns/hydra/core#');
+    });
+  });
+
+
 });
-
-
-
-
